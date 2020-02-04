f670c84b638db3facc0dc67b0207966342bebb7d
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-02-04 16:43:40 +0300

    add redux connect to PostList component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
+import { fetchPost } from "../actions";
+import { connect } from "react-redux";
 
 export class PostList extends Component {
+  componentDidMount() {
+    this.props.fetchPost();
+  }
+
   render() {
     return <div>PostList</div>;
   }
 }
+
+export default connect(null, { fetchPost })(PostList);
